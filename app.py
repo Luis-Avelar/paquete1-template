c6efb918cf2b00acbc9033e66205d2fc8e2f9860
Luis Avelar <Ing.luis.avelar7.7@gmail.com>
2026-06-11 15:10:10 -0500

    Agregar gunicorn para deploy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
+
 from database import (
     crear_tabla,
     guardar_prospecto,
@@ -7,7 +8,8 @@ from database import (
 )
 
 app = Flask(__name__)
-app.secret_key = "cambia-esta-clave-en-produccion"
+
+app.secret_key = "consultora_luis_2026_panel_privado_82918"
 
 crear_tabla()
 
@@ -19,24 +21,34 @@ def inicio():
 
 @app.route("/contacto", methods=["POST"])
 def contacto():
+
     nombre = request.form["nombre"]
     telefono = request.form["telefono"]
     correo = request.form.get("correo", "")
     mensaje = request.form["mensaje"]
 
-    guardar_prospecto(nombre, telefono, correo, mensaje)
+    guardar_prospecto(
+        nombre,
+        telefono,
+        correo,
+        mensaje
+    )
 
     return redirect("/#contacto")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
     if request.method == "POST":
+
         usuario = request.form["usuario"]
         password = request.form["password"]
 
         if usuario == "admin" and password == "1234":
+
             session["login"] = True
+
             return redirect("/panel")
 
     return render_template("login.html")
@@ -44,10 +56,12 @@ def login():
 
 @app.route("/panel")
 def panel():
+
     if not session.get("login"):
         return redirect("/login")
 
     prospectos = obtener_prospectos()
+
     total = len(prospectos)
 
     return render_template(
@@ -59,16 +73,20 @@ def panel():
 
 @app.route("/eliminar/<int:id>")
 def eliminar(id):
+
     if not session.get("login"):
         return redirect("/login")
 
     eliminar_prospecto(id)
+
     return redirect("/panel")
 
 
 @app.route("/logout")
 def logout():
+
     session.clear()
+
     return redirect("/login")
 
 
